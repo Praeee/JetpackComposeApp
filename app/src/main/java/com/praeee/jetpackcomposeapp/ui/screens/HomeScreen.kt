a8fb7e16aa2d903e2c3846db71054a39390d2999
pornpailin.cha <pornpailin.cha@axonstech.com>
2024-05-01 21:51:24 +0700

    add new view model, buildSrc

--- app/src/main/java/com/praeee/jetpackcomposeapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/praeee/jetpackcomposeapp/ui/screens/HomeScreen.kt
@@ -6,15 +6,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.praeee.jetpackcomposeapp.ui.theme.JetpackComposeAppTheme
+import com.praeee.jetpackcomposeapp.ui.viewmodel.NewsViewModel
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+//    newsViewModel: NewsViewModel = hiltViewModel()
+) {
     HomeScreenContent()
 }
 
 @Composable
-fun HomeScreenContent () {
+fun HomeScreenContent (
+//    newsViewModel: NewsViewModel
+) {
     Surface (
         modifier = Modifier
             .fillMaxSize()
