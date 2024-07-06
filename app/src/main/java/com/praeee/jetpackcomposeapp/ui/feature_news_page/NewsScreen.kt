3f2a9012c41eb98a1e2ce846a1f7c7af5007458b
pornpailin.cha <pornpailin.cha@axonstech.com>
2024-07-06 22:58:40 +0700

    add go to top lazy list

--- app/src/main/java/com/praeee/jetpackcomposeapp/ui/feature_news_page/NewsScreen.kt
+++ app/src/main/java/com/praeee/jetpackcomposeapp/ui/feature_news_page/NewsScreen.kt
@@ -3,6 +3,9 @@ package com.praeee.jetpackcomposeapp.ui.feature_news_page
 import android.annotation.SuppressLint
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,10 +13,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -26,8 +31,10 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import com.praeee.jetpackcomposeapp.R
 import com.praeee.jetpackcomposeapp.ui.components.BoxWithSwipeRefresh
 import com.praeee.jetpackcomposeapp.ui.components.ErrorUiState
+import com.praeee.jetpackcomposeapp.ui.components.GoToTop
 import com.praeee.jetpackcomposeapp.ui.components.Loader
 import com.praeee.jetpackcomposeapp.ui.components.TopAppBar
+import com.praeee.jetpackcomposeapp.ui.components.isScrollingUp
 import com.praeee.jetpackcomposeapp.ui.feature_news_page.widgets.ArticleItemCard
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -72,6 +79,9 @@ fun NewsScreenContent(
     val coroutineScope = rememberCoroutineScope()
 
     val articleList = state.articleList?.articleList ?: emptyList()
+
+    val listState = rememberLazyListState()
+    val scope = rememberCoroutineScope()
 
     Surface(
         modifier = Modifier
@@ -119,7 +129,8 @@ fun NewsScreenContent(
                         .fillMaxSize()
                 ) {
                     LazyColumn(
-                        modifier = modifier
+                        modifier = modifier,
+                        state = listState
                     ) {
 
                         itemsIndexed(
@@ -134,7 +145,17 @@ fun NewsScreenContent(
                         }
 
                     }
+                    Column {
+                        AnimatedVisibility(visible = !listState.isScrollingUp(), enter = fadeIn(), exit = fadeOut()) {
+                            GoToTop {
+                                scope.launch {
+                                    listState.scrollToItem(0)
+                                }
+                            }
+                        }
+                    }
                 }
+
             }
         }
     }
